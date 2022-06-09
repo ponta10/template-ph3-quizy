fbcac90cc9ce30cbb4773963f9bfb551e4f941c1
ponta <mponta2002@gmail.com>
2022-06-10 01:58:49 +0900

    練習

---
+++ src/resources/views/hello/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Hello/Index</title>
+     <style>
+          body{
+               font-size: 16pt;
+               color: #999;
+          }
+          h1{
+               font-size: 100pt;
+               text-align: right;
+               color: #f6f6f6;
+               margin: -40px 0px -80px 0px;
+          }
+     </style>
+</head>
+<body>
+     
+     <h1>Index</h1>
+     <p><?= $msg ;?></p>
+     <p>ID=<?= $id ;?></p>
+     <p><?= date("Y年n月j日") ;?></p>
+</body>
+</html>
